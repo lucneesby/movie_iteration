51ea26c91b79516772c24c84f25f5d51e8e11157
Luc Neesby <lucneesby3@gmail.com>
2017-06-12 18:15:53 -0700

    Create main.js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+var movies = [
+{
+	title: "Fight Club",
+	hasWatched: true,
+	rating: " - 4 Stars"
+
+},
+{
+	title: "Trumps United",
+	hasWatched: false,
+	rating: " - 5 stars",
+}
+]
+
+movies.forEach(function(masdf){
+	console.log(buildString(masdf));
+});
+
+function buildString (movie) {
+	var result = "You have ";
+	if (movie.hasWatched) {
+		result += "watched ";
+	} else {
+		result += "not seen ";
+	}
+	result += "\"" + movie.title + "\"";
+	result +=  movie.rating;
+	return result;
+}
 
